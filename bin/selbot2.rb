588bc000d28d1c280e7e2a3a3a4f533770a980a3
Jari Bakken <jari.bakken@gmail.com>
2011-10-12 01:20:40 +0200

    Cleanup, send :help as privmsg.

--- bin/selbot2.rb
+++ bin/selbot2.rb
@@ -20,37 +20,28 @@ Cinch::Bot.new {
     ]
   end
 
+  Selbot2::HELPS << [':help', "you're looking at it"]
   on :message, /:help/ do |m|
     just = Selbot2::HELPS.map { |e| e[0].length }.max
-    Selbot2::HELPS.each { |command, help| m.reply "#{command.ljust just} - #{help}" }
-  end
-
-  on :message, /:newissue/ do |m|
-    m.reply "http://code.google.com/p/selenium/issues/entry"
-  end
-
-  on :message, /:apidocs/ do |m|
-    m.reply ".NET: http://goo.gl/Fm3cw | Java: http://goo.gl/kKQqM | Ruby: http://goo.gl/cFyyT"
-  end
-
-  on :message, /:downloads/ do |m|
-    m.reply "http://seleniumhq.org/download/ and http://code.google.com/p/selenium/downloads/list"
-  end
-
-  on :message, /:gist/ do |m|
-    m.reply "Please paste >3 lines of text to http://gist.github.com"
-  end
-
-  on :message, /:ask/ do |m|
-    m.reply "If you have a question, please just ask it. Don't look for topic experts. Don't ask to ask. Don't PM. Don't ask if people are awake, or in the mood to help. Just ask the question straight out, and stick around. We'll get to it eventually :)"
-  end
-  
-  on :message, /:cla/ do |m|
-    m.reply "http://goo.gl/qC50R"
+    Selbot2::HELPS.each { |command, help| m.user.privmsg "#{command.ljust just} - #{help}" }
   end
 
   on :message, /:log/ do |m|
     m.reply "http://selenium.saucelabs.com/irc/logs/selenium.#{(Time.new.gmtime - 25200).strftime('%F')}.log"
+  end
+
+  [
+    [/:newissue/         , "http://code.google.com/p/selenium/issues/entry", "link to issue the tracker"],
+    [/:apidocs/          , ".NET: http://goo.gl/Fm3cw | Java: http://goo.gl/kKQqM | Ruby: http://goo.gl/cFyyT", "links to API docs"],
+    [/:downloads/        , "http://seleniumhq.org/download/ and http://code.google.com/p/selenium/downloads/list", "links to downloads pages"],
+    [/:gist/             , "Please paste >3 lines of text to http://gist.github.com", "link to gist.github.com", "link to Selenium's CLA"],
+    [/:ask/              , "If you have a question, please just ask it. Don't look for topic experts. Don't ask to ask. Don't PM. Don't ask if people are awake, or in the mood to help. Just ask the question straight out, and stick around. We'll get to it eventually :)", "Don't ask to ask."],
+    [/:cla/              , "http://goo.gl/qC50R", "don't ask to ask.", "link to today's chat log at saucelabs"],
+    [/:(mailing)?lists?/ , "https://groups.google.com/forum/#!forum/selenium-users | https://groups.google.com/forum/#!forum/selenium-developers", "link to mailing lists"],
+    [/:chrome(driver)?/  , "http://code.google.com/p/selenium/wiki/ChromeDriver | http://code.google.com/p/chromium/downloads/list", "link to ChromeDriver (wiki + downloads)"],
+  ].each do |exp, msg, help_text|
+    Selbot2::HELPS << [exp.source, help_text]
+    on(:message, exp) { |m| m.reply msg }
   end
 
 }.start
--- lib/selbot2.rb
+++ lib/selbot2.rb
@@ -21,12 +21,3 @@ require 'selbot2/notes'
 require 'selbot2/seen'
 require 'selbot2/seleniumhq'
 
-# find a way to not maintain this manually
-Selbot2::HELPS << [":newissue", "link to issue the tracker"]
-Selbot2::HELPS << [":apidocs", "links to API docs"]
-Selbot2::HELPS << [":downloads","link to the downloads page"]
-Selbot2::HELPS << [":gist", "link to gist.github.com"]
-Selbot2::HELPS << [":ask", "don't ask to ask."]
-Selbot2::HELPS << [":help", "you're looking at it"]
-Selbot2::HELPS << [":cla", "link to Selenium's CLA"]
-Selbot2::HELPS << [":log", "link to today's chat log at saucelabs"]
