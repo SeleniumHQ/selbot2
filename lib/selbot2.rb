751c90b63fe8ef75579ae72462a22d29787d6a63
Jari Bakken <jari.bakken@gmail.com>
2013-01-18 11:56:05 +0100

    there we go

--- lib/selbot2.rb
+++ lib/selbot2.rb
@@ -10,13 +10,11 @@ module Selbot2
 end
 
 require 'selbot2/helpers/jenkins'
-require 'selbot2/helpers/git'
 
 require 'selbot2/persistable'
 require 'selbot2/util'
 require 'selbot2/issues'
 require 'selbot2/revisions'
-require 'selbot2/commits'
 require 'selbot2/wiki'
 require 'selbot2/youtube'
 require 'selbot2/notes'
