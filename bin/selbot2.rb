16aba4d3b89fc67a55b54c40ab82964cf9862840
Andreas Tolfsen <ato@mozilla.com>
2014-12-08 19:22:39 +0000

    Shorthand for referecing good article on downloads and Selenium

--- bin/selbot2.rb
+++ bin/selbot2.rb
@@ -54,9 +54,14 @@ Cinch::Bot.new {
       :help       => "links to API docs"
     },
     {
-      :expression => /:downloads/,
+      :expression => /:download/,
       :text       => "http://seleniumhq.org/download/ and http://selenium-release.storage.googleapis.com/index.html",
       :help       => "links to downloads pages"
+    },
+    {
+      :expression => /:downloads/,
+      :text       => "Please read this article about how to download files with Selenium, and why you shouldn't: http://ardesco.lazerycode.com/index.php/2012/07/how-to-download-files-with-selenium-and-why-you-shouldnt/",
+      :help       => "links to article about downloading files"
     },
     {
       :expression => /:gist/,
