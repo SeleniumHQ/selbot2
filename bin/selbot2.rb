240f1a66af188c3037e2aacdf8b6d53efc25a3b2
James Reed <james.reed@blackcatsolutions.co.uk>
2018-05-01 11:47:16 +0100

    updating gist to use pastebin as gist no longer supports anonymous uploads

--- bin/selbot2.rb
+++ bin/selbot2.rb
@@ -69,8 +69,8 @@ Cinch::Bot.new {
       :help       => "links to article about downloading files"
     },
     {
-      :expression => /:gist/,
-      :text       => "Please paste >3 lines of code or text to https://gist.github.com and share the resulting URL",
+      :expression => /:(gist|paste|pastebin)/,
+      :text       => "Please paste >3 lines of code or text to https://pastebin.com and share the resulting URL",
       :help       => "link to gist.github.com",
     },
     {
